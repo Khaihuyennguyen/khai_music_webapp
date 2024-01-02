665977abe9a7b2afec2a5103c6a3b250d0ca01a6
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-01 21:23:30 -0600

    test modeul login

---
+++ kha-i/__test__/Login.spec.tsx
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Login from '../src/components/Login';
+
+describe('Login health check', () => {
+    it('Login renders successfully', () => {
+        render(
+            <Login />
+        );
+
+        expect(
+            screen.getByText(/Kha-i/)).toBeInTheDocument();
+    });
+    
+});
