984a0e819f08cba716f32145e907d4c38456425c
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-02 02:21:31 -0600

    add Spotify API

--- kha-i/src/components/App/index.tsx
+++ kha-i/src/components/App/index.tsx
@@ -1,16 +1,39 @@
 import Login from '../Login'
 import TrackInfo from '../Trackinfo';
 import {useState} from 'react';
+import { getAccessToken } from '../../auth'
+import { useEffect } from 'react'
+
+
 function App() {
   const [token, setToken] = useState<string | null>(null)
+
+  useEffect(  () => {
+    getToken()
+    
+  })
+
+  const getToken = async () => {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+      const accessToken = getAccessToken(clientId, code)
+      setToken(accessToken)
+    }
+  }
+
   if (!token) {
       return (
         <>
-          <Login></Login>
+          <Login/>
         </>
       )
   } else {
-    <TrackInfo></TrackInfo>
+    <>
+      <TrackInfo/>
+    </>
   }
 
   
--- kha-i/src/components/Nav/index.tsx
+++ kha-i/src/components/Nav/index.tsx
@@ -1,11 +1,14 @@
-
+import { redirectToAuthCodeFlow } from "../../auth";
 
 const Nav = () => {
-
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const handleClick = async () => {
+        await redirectToAuthCodeFlow(clientId);
+    }
     return (
         <>
             <h1>Kha-i</h1>
-            <button>Login</button>
+            <button onClick={handleClick}>Login</button>
         </>
         
     )
